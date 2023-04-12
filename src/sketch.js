a0a51a798349425412b53b44b23a4996f2641de5
i3games <i3games@users.noreply.github.com>
2023-04-13 00:07:45 +0200

    font

--- src/sketch.js
+++ src/sketch.js
@@ -1,20 +1,19 @@
 import { v4 as uuidv4 } from 'uuid';
 
 export const mySketch = (p) => {
-  let canv, capture;
-  let hold;
+  let canv, capture, hold, mainDiv;
   const id = uuidv4();
 
   // Calling p5.js functions, using the variable 'p'
   p.setup = () => {
-    let div = p.createDiv('').size(320, 240);
-    div.center();
+    mainDiv = p.createDiv('').size(320, 240);
+    mainDiv.center();
     canv = p.createCanvas(320, 240);
-    canv.parent(div);
-    let text = p.createP('To take a portrait, make sure that your head is inside the red oval and that you have good lighting. Then click on the preview image to download the photo to your computer. Repeat until you are happy with the result. No data is stored on this server.');
-    text.parent(div);
+    canv.parent(mainDiv);
+    let text = p.createP('To take a portrait, make sure that your head is inside the oval and that you have good lighting. Then click on the preview to download the portrait to your computer. Repeat until you are happy with the result. No data or image is stored on this server.');
+    text.parent(mainDiv);
     capture = p.createCapture(p.VIDEO);
-    console.log(capture.width, capture.height); // definitely wrong, assume 720p 16:9
+    // console.log(capture.width, capture.height); // definitely wrong, assume 720p 16:9
     capture.hide();
     hold = p.createImage(1280, 720);
   }
@@ -24,7 +23,7 @@ export const mySketch = (p) => {
     p.image(capture, 0, 0, 320, 240);
     //p.image(hold, 400, 0);
     p.noFill();
-    p.stroke(255, 12, 0);
+    p.stroke(255, 136, 0);
     p.strokeWeight(4);
     p.ellipse(160, 120, 150, 200);
   }
@@ -35,7 +34,10 @@ export const mySketch = (p) => {
     // restart the camera so it keeps showing the live feed
     capture = p.createCapture(p.VIDEO);
     capture.hide();
+  }
 
+  p.windowResized = () =>{
+    mainDiv.center();
   }
 
 }
